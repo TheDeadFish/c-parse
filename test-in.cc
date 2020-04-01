d5bf1985475951b6fa759022102a9de52b6e9a27
TheDeadFish <michaelstamper1@gmail.com>
2020-04-01 06:35:55 +0000

    1. added test-in.cc: last modified 2017, never commited

---
+++ test-in.cc
@@ -0,0 +1,17 @@
++=
+#ifdef test
+ #define fred ( x ) fred ( j) poop
+ #define joe+ + +
+ #undef joe+ + +
+ #include "stdio.h"
+ #include <shit.h>
+ hello you shit
+#else
+ fish
+ #ifdef chips
+  sdfs #hello
+   sdf 
+ #else 
+  [pp[sdf
+ #endif
+#endif
